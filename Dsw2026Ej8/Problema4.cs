ab00817ad9beda8b2ede5c33ea2e6e89e590133e
Matias <matiaskuhn.93@gmail.com>
2026-05-02 18:58:17 -0300

    Problema N4 - Solución

---
+++ Dsw2026Ej8/Problema4.cs
@@ -0,0 +1,16 @@
+﻿namespace Dsw2026Ej8
+{
+    public class Problema4
+    {
+        public double CalcularPromedio(int? nota1, int? nota2, int? nota3)
+        {
+            var notas = new int?[] { nota1, nota2, nota3 };
+            var notasValidas = notas.Where(nota => nota.HasValue && nota.Value > 0 && nota.Value <= 10);
+
+            return !notasValidas.Any()
+                ? 0
+                : (double) notasValidas.Average(x => x.Value);
+
+        }
+    }
+}
--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -17,10 +17,15 @@
             //Console.WriteLine(problema2.CrearResumenVenta(codigo, descripcion, cantidad, precio));
 
             //Ejercicio N3
-            var numero = 1;
-            var product = new Product() { Description = "Old description" };
-            var problema3 = new Problema3();
-            Console.WriteLine(problema3.CompararCopias(numero, product));
+            //var numero = 1;
+            //var product = new Product() { Description = "Old description" };
+            //var problema3 = new Problema3();
+            //Console.WriteLine(problema3.CompararCopias(numero, product));
+
+            //Ejercicio N4
+            //var problema4 = new Problema4();
+            //double resultado = problema4.CalcularPromedio(null, 10, 5);
+            //Console.WriteLine(resultado);
         }
     }
 
