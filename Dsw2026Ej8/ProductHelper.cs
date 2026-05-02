b4f7ced6bc8d39fbc702314abdb15a7415a08484
Matias <matiaskuhn.93@gmail.com>
2026-05-02 17:14:51 -0300

    Problema N1 - Solución y README.md

---
+++ Dsw2026Ej8/ProductHelper.cs
@@ -0,0 +1,8 @@
+﻿namespace Dsw2026Ej8
+{
+    public static partial class ProductHelper
+    {
+        public static string ObtenerEtiquetaProducto(long code, string description, decimal price)
+        => $"[{code}] {description} - {AgregarFormatoAlPrecio(price)}";
+    }
+}
--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -1,10 +1,16 @@
 ﻿namespace Dsw2026Ej8
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(ProductHelper.ObtenerEtiquetaProducto(0001, "Yerba Mate RosaMonte", 500m));
         }
+    }
+
+    public static partial class ProductHelper
+    {
+        public static string AgregarFormatoAlPrecio(decimal price) =>
+            price.ToString("C2", new System.Globalization.CultureInfo("es-AR"));
     }
 }
